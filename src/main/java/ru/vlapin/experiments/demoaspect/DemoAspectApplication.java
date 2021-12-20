f49f0d23b455b72798436136ecf8f675771086b9
VLapin <Vyacheslav.Lapin@gmail.com>
2021-12-20 12:56:51 +0300

    Initial commit

---
+++ src/main/java/ru/vlapin/experiments/demoaspect/DemoAspectApplication.java
@@ -0,0 +1,12 @@
+package ru.vlapin.experiments.demoaspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAspectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoAspectApplication.class, args);
+  }
+}
