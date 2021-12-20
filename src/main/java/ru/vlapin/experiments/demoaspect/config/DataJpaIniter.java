f3bf922f3384435eb6c516f577395c97a0760a53
VLapin <Vyacheslav.Lapin@gmail.com>
2021-12-20 12:57:39 +0300

    small changes

--- src/main/java/ru/vlapin/experiments/demoaspect/config/DataJpaIniter.java
+++ src/main/java/ru/vlapin/experiments/demoaspect/config/DataJpaIniter.java
@@ -1,20 +1,25 @@
 package ru.vlapin.experiments.demoaspect.config;
 
+import static ru.vlapin.experiments.demoaspect.common.Loggable.LogLevel.INFO;
+
 import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import ru.vlapin.experiments.demoaspect.common.Loggable;
 import ru.vlapin.experiments.demoaspect.dao.CatRepository;
 import ru.vlapin.experiments.demoaspect.model.Cat;
 
 @Component
+@Loggable(INFO)
 @RequiredArgsConstructor
 public class DataJpaIniter implements ApplicationRunner {
 
   CatRepository catRepository;
 
   @Override
+//  @Loggable(INFO)
   public void run(ApplicationArguments __) {
     Stream.of("Мурзик, Барсик, Матроскин".split(", "))
         .map(Cat::new)
